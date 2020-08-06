48713aaaa1348c836a8ac95625921942bd86a88f
Chunyu Ma <machunyu4402@hotmail.com>
2020-08-06 17:32:04 -0400

    #994 add new script to pull graph for the canonicalized new kg2

---
+++ code/ARAX/ARAXQuery/Overlay/GraphSage_train/py_scripts/pull_canonicalized_KG2.py
@@ -0,0 +1,39 @@
+import sys, os
+import pandas as pd
+from neo4j import GraphDatabase
+pathlist = os.path.realpath(__file__).split(os.path.sep)
+RTXindex = pathlist.index("RTX")
+sys.path.append(os.path.sep.join([*pathlist[:(RTXindex + 1)], 'code']))
+from RTXConfiguration import RTXConfiguration
+
+output_path = '~/work/RTX/code/reasoningtool/MLDrugRepurposing/Test_graphsage/kg2canonicalized_data/'
+
+## Connect to neo4j database
+rtxc = RTXConfiguration()
+driver = GraphDatabase.driver("bolt://kg2canonicalized.rtx.ai:7687", auth=(rtxc.neo4j_username, rtxc.neo4j_password))
+session = driver.session()
+
+## Pull a dataframe of all of the graph edges
+query = 'match (n)-[r]-(m) where m<>n and type(r)<>"contraindicated_for" and type(r)<>"indicated_for" with distinct n as node1, m as node2 return node1.id as source, node2.id as target'
+res = session.run(query)
+KG2_alledges = pd.DataFrame(res.data())
+KG2_alledges.to_csv(output_path + 'graph_edges.txt', sep='\t', index=None)
+
+## Pulls a dataframe of all of the graph nodes with category label
+query = "match (n) with distinct n.id as id, n.name as name, n.preferred_type as category return id, name, category"
+res = session.run(query)
+KG2_allnodes_label = pd.DataFrame(res.data())
+KG2_allnodes_label = KG2_allnodes_label.iloc[:, [0, 2]]
+KG2_allnodes_label.to_csv(output_path + 'graph_nodes_label_remove_name.txt', sep='\t', index=None)
+
+## Pulls a dataframe of all of the graph drug-associated nodes
+query = f"match (n:chemical_substance) with distinct n.id as id, n.name as name return id, name union match (n:drug) with distinct n.id as id, n.name as name return id, name"
+res = session.run(query)
+drugs = pd.DataFrame(res.data())
+drugs.to_csv(output_path + 'drugs.txt', sep='\t', index=None)
+
+## Pulls a dataframe of all of the graph disease and phenotype nodes
+query = "match (n:phenotypic_feature) with distinct n.id as id, n.name as name return id, name union match (n:disease) with distinct n.id as id, n.name as name return id, name"
+res = session.run(query)
+diseases = pd.DataFrame(res.data())
+diseases.to_csv(output_path + 'diseases.txt', sep='\t', index=None)
