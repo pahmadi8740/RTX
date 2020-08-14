f1817f439221e096ee3a65a454a66fb633cb20a5
Chunyu Ma <machunyu4402@hotmail.com>
2020-08-14 11:32:24 -0400

    #994 modify the neo4j query to filter out more edges between drug and disease

--- code/ARAX/ARAXQuery/Overlay/GraphSage_train/py_scripts/pull_canonicalized_KG2.py
+++ code/ARAX/ARAXQuery/Overlay/GraphSage_train/py_scripts/pull_canonicalized_KG2.py
@@ -14,7 +14,7 @@ driver = GraphDatabase.driver("bolt://kg2canonicalized.rtx.ai:7687", auth=(rtxc.
 session = driver.session()
 
 ## Pull a dataframe of all of the graph edges
-query = 'match (n)-[r]-(m) where m<>n and type(r)<>"contraindicated_for" and type(r)<>"indicated_for" with distinct n as node1, m as node2 return node1.id as source, node2.id as target'
+query = 'match (n)-[r]-(m) where m<>n and type(r)<>"contraindicated_for" and type(r)<>"indicated_for" and type(r)<>"treats" and type(r)<>"prevents" and type(r)<>"causes" and type(r)<>"contraindicated_with" and type(r)<>"intent_to_treat" and type(r)<>"disease_responds_to" and type(r)<>"antagonist" and type(r)<>"disrupts" and type(r)<>"has_pharmaceutical_state_of_matter" and type(r)<>"increase" and type(r)<>"has_pharmaceutical_basic_dose_form" and type(r)<>"chemical_or_drug_has_mechanism_of_action" and type(r)<>"contributes_to" and type(r)<>"predisposes_towards" and type(r)<>"regulates_process_to_process" and type(r)<>"positively_regulates_entity_to_entity" and type(r)<>"negatively_regulates_entity_to_entity" with distinct n as node1, m as node2 return node1.id as source, node2.id as target'
 res = session.run(query)
 KG2_alledges = pd.DataFrame(res.data())
 KG2_alledges.to_csv(output_path + 'graph_edges.txt', sep='\t', index=None)
